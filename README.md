7a0fbb8c644862db05eed32f5ddc6f363eb0306b
datngo <dat821168@gmail.com>
2022-06-10 22:35:31 +0700

    update README and train script

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Train process
 ```bash
-python vfastpunct/trainer.py train --model_arch lstm_softmax --model_name_or_path bert-base-multilingual-cased --learning_rate 1e-3 --max_seq_length 190 --epochs 100 --train_batch_size 32 --eval_batch_size 16 --data_dir datasets/ --output_dir outputs/
+python trainer.py train --data_dir datasets/samples --model_name_or_path vinai/phobert-base --model_arch softmax --output_dir outputs --max_seq_length 256 --train_batch_size 1 --eval_batch_size 1 --learning_rate 5e-5 --epochs 3
 ```
 
 or
--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@
 
 pip install -r requirements.txt
 export PYTHONPATH=.
-python3 trainer.py train --data_dir datasets/samples --model_name_or_path vinai/phobert-base --model_arch softmax --output_dir outputs --max_seq_length 256 --train_batch_size 1 --eval_batch_size 1 --learning_rate 5e-5 --epochs 3
+python3 trainer.py train train --data_dir datasets/samples --model_name_or_path vinai/phobert-base --model_arch softmax --output_dir outputs --max_seq_length 256 --train_batch_size 1 --eval_batch_size 1 --learning_rate 5e-5 --epochs 3
