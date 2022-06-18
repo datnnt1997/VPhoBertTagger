d0695070570729bfd429fe9923bdc80431674daa
datngo <dat821168@gmail.com>
2022-06-18 08:29:11 +0700

    dataset vlsp

--- tools/make_exp1_data.sh
+++ tools/make_exp1_data.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-trainDir="./data/VLSP2018-NER-train-Jan14"
-devDir="./data/VLSP2018-NER-dev"
-testDir="./data/VLSP2018-NER-Test-Domains"
+trainDir="../datasets/vlsp2018/raw/VLSP2018-NER-train-Jan14"
+devDir="../datasets/vlsp2018/raw/VLSP2018-NER-dev"
+testDir="../datasets/vlsp2018/raw/VLSP2018-NER-Test-Domains"
 
 # For training data
-dataDir="./data/exp1/train"
+dataDir="../datasets/vlsp2018/exp1/train"
 
 mkdir -p $dataDir
 
@@ -17,7 +17,7 @@ cut -f 1,4 -d " " $dataDir/train_ws.txt > $dataDir/train_ws-l3.txt
 python join_tags2.py -o $dataDir/train_ws-l1+l2.txt $dataDir/train_ws-l1.txt $dataDir/train_ws-l2.txt
 
 # For devset
-dataDir="./data/exp1/dev"
+dataDir="../datasets/vlsp2018/exp1/dev"
 
 mkdir -p $dataDir
 
@@ -29,7 +29,7 @@ cut -f 1,4 -d " " $dataDir/dev_ws.txt > $dataDir/dev_ws-l3.txt
 python join_tags2.py -o $dataDir/dev_ws-l1+l2.txt $dataDir/dev_ws-l1.txt $dataDir/dev_ws-l2.txt
 
 # For test set
-dataDir="./data/exp1/test"
+dataDir="../datasets/vlsp2018/exp1/test"
 
 mkdir -p $dataDir
 
