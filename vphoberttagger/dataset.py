014e4808d4a9ce18820b2e04fe7a46cd1e84028d
datngo <dat821168@gmail.com>
2022-06-20 15:30:49 +0700

    fix model lstm_crf and add model crf

--- vphoberttagger/dataset.py
+++ vphoberttagger/dataset.py
@@ -44,8 +44,6 @@ def convert_examples_features(data_path: Union[str, os.PathLike],
                 continue
         seq_len = len(tokens)
         sentence = ' '.join(tokens)
-        LOGGER.info(sentence)
-        LOGGER.info(tag_ids)
         encoding = tokenizer(sentence,
                              padding='max_length',
                              truncation=True,
@@ -64,7 +62,7 @@ def convert_examples_features(data_path: Union[str, os.PathLike],
                 valid_ids[idx+1] = 1
             valid_labels[idx+1] = tag_ids[i-1]
             i += 1
-        if max_seq_len > seq_len + 2:
+        if max_seq_len >= seq_len + 2:
             label_padding_size = (max_seq_len - seq_len)
             label_marks[:seq_len] = [1] * seq_len
             tag_ids.extend([0] * label_padding_size)
