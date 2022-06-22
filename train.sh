06927e48298212178fde3c8dd16cd8878d24b61a
datngo <dat821168@gmail.com>
2022-06-22 15:31:16 +0700

    update train script and README

--- train.sh
+++ train.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-pip install -r requirements.txt
 export PYTHONPATH=.
 python3 main.py train --task vlsp2016 --run_test --data_dir ./datasets/vlsp2016 --model_name_or_path vinai/phobert-base --model_arch softmax --output_dir outputs --max_seq_length 256 --train_batch_size 32 --eval_batch_size 32 --learning_rate 3e-5 --epochs 20 --early_stop 2 --overwrite_data
